d3f6f6db7107fe50732019922d454e29dda2e497
Sayan Porya <sayanporya@gmail.com>
2020-05-25 11:27:39 +0530

    on the way to add test

---
+++ app/src/test/java/com/eco4ndly/githubtrendingrepo/repolist/RepoListViewModelTest.kt
@@ -0,0 +1,9 @@
+package com.eco4ndly.githubtrendingrepo.repolist
+
+import org.koin.test.AutoCloseKoinTest
+
+/**
+ * A Sayan Porya code on 25/05/20
+ */
+class RepoListViewModelTest: AutoCloseKoinTest() {
+}
