7c6455048b8b030abfafcca38d4c2397d7ae44e8
Sayan Porya <sayanporya@gmail.com>
2020-05-25 11:28:22 +0530

    added comment on public method

--- app/src/main/java/com/eco4ndly/githubtrendingrepo/domain/mapper/Mapper.kt
+++ app/src/main/java/com/eco4ndly/githubtrendingrepo/domain/mapper/Mapper.kt
@@ -5,13 +5,22 @@ import kotlinx.coroutines.withContext
 
 /**
  * A Sayan Porya code on 24/05/20
+ *
+ * Interface to be implemented data class mapper classes
  */
 interface Mapper<Output> {
+
+  /**
+   * Maps using getMapping() function
+   */
   suspend fun map(): Output {
     return withContext(Dispatchers.Default) {
       getMapping()
     }
   }
 
+  /**
+   * Gets the mapping of Input -> Output to use in [map()] function
+   */
   fun getMapping(): Output
 }
